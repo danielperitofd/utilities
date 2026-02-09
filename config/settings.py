01557fc010e49d0f2a4e5ae869fddd76018459e3
danielperitofd <danielperitofd@gmail.com>
2026-02-09 17:20:13 -0300

    Django deploy prep: unify requirements, add Procfile/runtime, media rendering fix, .gitignore

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
             ],
         },
     },
